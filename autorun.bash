cfaeee816a8bd98ef3903b150a8e5058a8e7c0b8
Liam Ralph <liamralph@tutamail.com>
2025-09-07 19:32:33 -0400

    start adding loading bar for autorun

--- autorun.bash
+++ autorun.bash
@@ -15,35 +15,56 @@ for i in "${tasks[@]}"; do
 
     IFS=":" read -ra task_pieces <<< "$i"
     reps="$((task_pieces[0]))" # Repetitions
-    savepng="${task_pieces[1]}" # Whether to save the png file
-    inputs="${task_pieces[2]}" # Python automode inputs
+    rep_times="${task_pieces[1]}" # Whether to list each rep's runtime
+    save_png="${task_pieces[2]}" # Whether to save the png file
+    inputs="${task_pieces[3]}" # Python automode inputs
 
     echo "Running task $inputs for $reps repititions."
 
-    if [ "$savepng" = "y" ]; then
+    if [ "$save_png" = "y" ]; then
         inputs="${inputs/".png"/"0.png"}" # Prep png path for saving multiple files
     fi
 
     # Run reps, calculate sum
 
     sum=0
+    if [ "$rep_times" = "n" ]; then
+        echo "1"
+    fi
+
     for ii in $(seq 1 $reps); do
-        if [ "$savepng" = "y" ]; then
+
+        if [ "$save_png" = "y" ]; then
             inputs="${inputs/"$((($ii) - 1)).png"/"$ii.png"}"
             # Rep 1 saved in file1.png, rep 2 in file2.png, etc.
         fi
+
         result=$(python3 main.py $inputs) # Run Python program
-        echo "Rep $ii Time: $(printf "%.4f" "$result")"
+
+        if [ "$rep_times" = "y" ]; then
+            echo "Rep $ii Time: $(printf "%.4f" "$result")" # Rep 1 Time: 1.0000
+        elif [ "$ii" != 0 ]; then
+            num_blocks=$((20 * $ii / $reps))
+            printf "\r\033[K\u001b[48;5;2m"
+            printf " %.0s" $(seq 1 "$num_blocks")
+            printf "\u001b[0m"
+        fi
+
         sum=$(echo "scale=6; $sum + $result" | bc)
+
     done
 
-    if [ "$savepng" = "n" ]; then
+    if [ "$save_png" = "n" ]; then
         # Get file path and remove file
         IFS=" " read -ra input_pieces <<< "$inputs"
         rm "${input_pieces[7]}"
     fi
 
     # Print average time
+
+    if [ "$rep_times" = "n" ]; then
+        echo ""
+    fi
 
     echo "Average Time: $(echo "scale=4; $sum / $reps" | bc)"
     echo ""
