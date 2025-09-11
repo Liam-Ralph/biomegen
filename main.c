d55f5835d0db4f9f7ca949d2ed8289deeba98a0e
Liam Ralph <liamralph@tutamail.com>
2025-09-11 15:41:22 -0400

    add starting message

--- main.c
+++ main.c
@@ -16,14 +16,17 @@ BiomeGen, a terminal application for generating png maps.
 
 // Includes
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 // Definitions
 
-#define ANSI_GREEN "\u001b[38;5;2m"
-#define ANSI_BLUE "\u001b[38;5;4m"
-#define ANSI_RESET "\u001b[0m"
+#define ANSI_GREEN "\003[38;5;2m"
+#define ANSI_BLUE "\003[38;5;4m"
+#define ANSI_RESET "\003[0m"
 
 
 // Structs
@@ -39,6 +42,14 @@ typedef struct Dot Dot;
 // Functions
 // (Alphabetical order)
 
+int clear_screen(){
+    char command[] = "clear";
+    #ifdef _WIN32
+        command = "cls";
+    #endif
+    system(command);
+}
+
 
 // Multiprocessing Functins
 //(Order of use)
@@ -47,6 +58,64 @@ typedef struct Dot Dot;
 // Main Function
 
 int main(int argc, char *argv[]) {
+
+    // Clearing Errors File
+
+    FILE *fptr;
+    fptr = fopen("errors.txt", "w");
+    fclose(fptr);
+
+    bool auto_mode;
+    char *output_file;
+
+    if (argc == 1) {
+        
+        // Manual Inputs Mode
+
+        auto_mode = false;
+
+        output_file = "result.png";
+
+        // Getting Program Version
+
+        char file_line[100];
+        fptr = fopen("README.md", "r");
+        fgets(file_line, 100, fptr);
+        fgets(file_line, 100, fptr);
+        fgets(file_line, 100, fptr);
+        fclose(fptr);
+
+        char version[16];
+        strncpy(version, file_line + 12, 12);
+        version[strlen(version) - 1] = '\0';
+
+        // Copyright, license notice, etc.
+        printf(
+            "Welcome to BiomeGen v%s\n"
+            "Copyright (C) 2025 Liam Ralph\n"
+            "https://github.com/liam-ralph\n"
+            "This project is licensed under the GNU General Public License v3.0,\n"
+            "except for result.png, this program's output, licensed under The Unlicense.\n"
+            "\003[38;5;1mWARNING: In some terminals, the refreshing progress screen\n"
+            "may flash, which could cause problems for people with epilepsy.%s\n"
+            "Press ENTER to begin.\n", version, ANSI_RESET
+        );
+        /*
+        I do not know if the flashing lights this program sometimes makes could reasonably cause
+        epilepsy or not, but I put this just in case
+        */
+        getchar();
+        clear_screen();
+
+        printf("Map Width (pixels):");
+
+    } else {
+        
+        // Automated Inputs Mode
+
+        auto_mode = true;
+
+    }
 
     return 0;
 
